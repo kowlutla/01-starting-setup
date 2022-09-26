e4ebf283e1189a4a59bc7e92f91843232b42f52d
Kowlutla <kowlutla1119@gmail.com>
2022-09-26 19:52:51 +0530

    useState to change Expense title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,17 @@
 import "./ExpenseItem.css";
+import React,{useState} from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card"
 
 const ExpenseItem = (props) =>{
-  const expenseTitle = props.expenseTitle;
   const expenseAmount = props.expenseAmount;
+  const[expenseTitle,setExpenseTitle]=useState(props.expenseTitle);
+
+  const clickHandler =()=>{
+    setExpenseTitle("Updated!"+expenseTitle);
+    console.log(expenseTitle);
+
+  }
 
   return (
     <Card className="expense-item">
@@ -13,6 +20,8 @@ const ExpenseItem = (props) =>{
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">{expenseAmount}</div>
       </div>
+
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
